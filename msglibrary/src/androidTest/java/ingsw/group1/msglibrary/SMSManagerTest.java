a8718476d8c97077af35f82d195d0f7ef7c234b6
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2019-12-21 11:03:19 +0100

    Added tests, ensured positive results

---
+++ msglibrary/src/androidTest/java/ingsw/group1/msglibrary/SMSManagerTest.java
@@ -0,0 +1,100 @@
+package ingsw.group1.msglibrary;
+
+import android.content.Context;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ingsw.group1.msglibrary.exceptions.InvalidAddressException;
+import ingsw.group1.msglibrary.exceptions.InvalidMessageException;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @author Giorgia Bortoletti
+ */
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class SMSManagerTest {
+
+    private SMSManager smsManager;
+    private static final String EX_VALID_ADDRESS = "+39892424";
+
+    @Before
+    public void createSmsManager(){
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+        this.smsManager = SMSManager.getInstance(context);
+    }
+
+    @Test
+    public void sendMessage_bodyEmpty() {
+        boolean sending = true;
+        try {
+            SMSPeer peer = new SMSPeer(EX_VALID_ADDRESS);
+            String data = "";
+            SMSMessage smsMessage = new SMSMessage(peer, data);
+            this.smsManager.sendMessage(smsMessage);
+        }catch(InvalidMessageException e){
+            sending = false;
+        }
+        assertFalse(sending);
+    }
+
+    @Test
+    public void sendMessage_peerEmpty() {
+        boolean sending = true;
+        try{
+            SMSPeer peer = new SMSPeer("");
+            String data = "body";
+            SMSMessage smsMessage = new SMSMessage(peer, data);
+            this.smsManager.sendMessage(smsMessage);
+        }catch(InvalidAddressException e){
+            sending = false;
+        }
+        assertFalse(sending);
+    }
+
+    @Test
+    public void sendMessage_peerBlank() {
+        boolean sending = true;
+        try{
+            SMSPeer peer = new SMSPeer("  ");
+            String data = "body";
+            SMSMessage smsMessage = new SMSMessage(peer, data);
+            this.smsManager.sendMessage(smsMessage);
+        }catch(InvalidAddressException e){
+            sending = false;
+        }
+        assertFalse(sending);
+    }
+
+    /**
+     * Peer not valid as global phone number
+     */
+    @Test
+    public void sendMessage_peerNotValid() {
+        boolean sending = true;
+        try{
+            SMSPeer peer = new SMSPeer("{{@@");
+            String data = "body";
+            SMSMessage smsMessage = new SMSMessage(peer, data);
+            this.smsManager.sendMessage(smsMessage);
+        }catch(InvalidAddressException e){
+            sending = false;
+        }
+        assertFalse(sending);
+    }
+
+    @Test
+    public void sendMessage_valid() {
+        SMSPeer peer = new SMSPeer(EX_VALID_ADDRESS);
+        String data = "body";
+        SMSMessage smsMessage = new SMSMessage(peer, data);
+        //TODO? not sendMessage because it does not have SEND_SMS permission
+        assertTrue(this.smsManager.sendMessage(smsMessage));
+    }
+}
---
+++ msglibrary/src/androidTest/java/ingsw/group1/msglibrary/SmsUtilsTest.java
@@ -0,0 +1,56 @@
+package ingsw.group1.msglibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Riccardo De Zen
+ */
+public class SmsUtilsTest {
+
+    private String formattedNumber = "+393928402034";
+    private String validNumber = "3928402034";
+    private String invalidNumber = "12345";
+    private String validCountryCode = "IT";
+    private String invalidCountryCode = "it";
+
+    @Test
+    public void formatSMSNumberNullNumber() {
+        assertNull(SmsUtils.formatSMSNumber(null, null));
+        assertNull(SmsUtils.formatSMSNumber(null,validCountryCode));
+    }
+
+    @Test
+    public void formatSMSNumberNullCountryCode() {
+        assertEquals(invalidNumber,SmsUtils.formatSMSNumber(invalidNumber, null));
+        assertEquals(validNumber,SmsUtils.formatSMSNumber(validNumber,null));
+    }
+
+    @Test
+    public void formatSMSNumberInvalidCountryCode() {
+        assertEquals(invalidNumber,SmsUtils.formatSMSNumber(invalidNumber, invalidCountryCode));
+        assertEquals(validNumber, SmsUtils.formatSMSNumber(validNumber,invalidCountryCode));
+    }
+
+    @Test
+    public void formatSMSNumberValidCountryCode() {
+        assertEquals(invalidNumber,SmsUtils.formatSMSNumber(invalidNumber,validCountryCode));
+        assertEquals(formattedNumber, SmsUtils.formatSMSNumber(validNumber, validCountryCode));
+    }
+
+    @Test
+    public void isMessagePertinentTrue(){
+        String message = SMSHandler.APP_KEY;
+        assertTrue(SmsUtils.isMessagePertinent(message));
+    }
+
+    @Test
+    public void isMessagePertinentFalse(){
+        String message = "Hello" + SMSHandler.APP_KEY;
+        assertFalse(SmsUtils.isMessagePertinent(message));
+    }
+}
