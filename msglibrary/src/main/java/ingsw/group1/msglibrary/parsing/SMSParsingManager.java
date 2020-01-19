a333ce2005cc9cfbf29a455b4713b9ce4faf4e3d
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-01-19 13:00:53 +0100

    Added SMSParsingManager tests.
    - Two tests are missing
    - Two tests require a fix due to unexplainable behaviour in mocked SharedPreferences
    - Changed JDK version to 1.8

--- msglibrary/src/main/java/ingsw/group1/msglibrary/parsing/SMSParsingManager.java
+++ msglibrary/src/main/java/ingsw/group1/msglibrary/parsing/SMSParsingManager.java
@@ -2,12 +2,11 @@ package ingsw.group1.msglibrary.parsing;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.ArrayMap;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Map;
 
 import ingsw.group1.msglibrary.SMSMessage;
@@ -18,8 +17,14 @@ import ingsw.group1.msglibrary.SMSMessage;
  * By default also acts as a parser that does no operation on the messages. Allows setting a new
  * parser to override the one in use by the current instance by calling
  * {@link SMSParsingManager#setOverridingParser(Class)}, such a change persists, since it is
- * stored in {@link android.content.SharedPreferences}, with a key which is:
- * {@link SMSParsingManager#PREF_KEY} + {@link SMSParsingManager#instanceName}.
+ * stored in {@link android.content.SharedPreferences}, in a file called
+ * {@link SMSParsingManager#PREF_FILENAME} with a key which is:
+ * {@link SMSParsingManager#PREF_KEY_PREFIX} + {@link SMSParsingManager#instanceName}.
+ * <p>
+ * In order to be instantiated the Parser class needs a public constructor with no parameters.
+ * Since the raw class name is stored, generics are not supported. If you need one of the
+ * previously stated features, move the parsing logic to an upper level.
+ * <p>
  * Also allows resetting to the default parsing strategy by calling
  * {@link SMSParsingManager#resetOverridingParser()}.
  *
@@ -30,13 +35,16 @@ public class SMSParsingManager implements SMSParser {
     //Base name for Preference file name.
     static final String PREF_FILENAME = "ingsw.group1.msglibrary.parsing.SMSParsingManager";
     //Base name for Preference key. Should be followed by instance name.
-    static final String PREF_KEY = "defaultParser-";
+    static final String PREF_KEY_PREFIX = "defaultParser-";
     //Map containing the active instances of this class.
-    private static Map<String, SMSParsingManager> activeInstances = new ArrayMap<>();
+    private static Map<String, SMSParsingManager> activeInstances = new HashMap<>();
 
-    private WeakReference<Context> context;
+    private final String PREF_KEY;
     private SharedPreferences parsingPreferences;
     private String instanceName;
+
+    @Nullable
+    private SMSParser overridingParser;
 
     /**
      * Private constructor for this class.
@@ -45,9 +53,10 @@ public class SMSParsingManager implements SMSParser {
      * @param instanceName The name for this instance. Must not be {@code null}.
      */
     private SMSParsingManager(@NonNull Context context, @NonNull String instanceName) {
-        this.context = new WeakReference<>(context.getApplicationContext());
-        this.parsingPreferences = context.getSharedPreferences(PREF_FILENAME, Context.MODE_PRIVATE);
+        this.parsingPreferences = getPreferences(context);
         this.instanceName = instanceName;
+        this.PREF_KEY = PREF_KEY_PREFIX + instanceName;
+        this.overridingParser = instantiateOverridingParser();
     }
 
     /**
@@ -70,22 +79,116 @@ public class SMSParsingManager implements SMSParser {
     }
 
     /**
-     * @param newParser A reference to the class defining the new overriding parser.
+     * Getter for {@link SMSParsingManager#instanceName}.
+     *
+     * @return {@link SMSParsingManager#instanceName}, the name for this instance.
+     */
+    @NonNull
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    /**
+     * Method to retrieve the {@link SharedPreferences}.
+     *
+     * @param context The {@link Context} from which to retrieve the {@link SharedPreferences}.
+     * @return The appropriate instance of {@link SharedPreferences}.
+     */
+    private SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(
+                PREF_FILENAME,
+                Context.MODE_PRIVATE
+        );
+    }
+
+    /**
+     * Method to save a value in the appropriate field {@link SharedPreferences}.
+     * Apply is used as suggested by the compiler in order to write to Preferences in the
+     * background, to let Android framework handle failures properly.
+     *
+     * @param newValue The new value for the field, can be null.
+     * @return The previously stored value.
      */
     @Nullable
-    public <P extends SMSParser> Class<P> setOverridingParser(@NonNull Class<P> newParser) {
-        return null;
+    private String storeInPreferences(@Nullable String newValue) {
+        String oldValue = parsingPreferences.getString(PREF_KEY, null);
+        SharedPreferences.Editor prefEditor = parsingPreferences.edit();
+        prefEditor.putString(PREF_KEY, newValue);
+        prefEditor.apply();
+        return oldValue;
+    }
+
+    /**
+     * Method setting the new Parser, this method saves the class' name in the
+     * {@link SharedPreferences}, in a file called {@link SMSParsingManager#PREF_FILENAME}, in a
+     * field called: {@link SMSParsingManager#PREF_KEY_PREFIX} +
+     * {@link SMSParsingManager#instanceName}.
+     *
+     * @param newParser A reference to the class defining the new overriding parser.
+     * @return The previously stored class, no type check are performed on the stored class
+     * except whether it extends {@link SMSParser}. Returns {@code null} if the class could not
+     * be found or it did not extend {@link SMSParser}.
+     */
+    @Nullable
+    public Class<? extends SMSParser> setOverridingParser(@NonNull Class<? extends SMSParser> newParser) {
+        Class<? extends SMSParser> oldStoredClass = getOverridingParser();
+        storeInPreferences(newParser.getName());
+        overridingParser = instantiateOverridingParser();
+        return oldStoredClass;
+    }
+
+    /**
+     * Getter for the saved parser class.
+     *
+     * @return The reference to the currently set {@link SMSParser} class. The Type of stored
+     * class is not questioned (as long as it extends {@link SMSParser}).
+     */
+    @Nullable
+    public Class<? extends SMSParser> getOverridingParser() {
+        String storedValue = parsingPreferences.getString(PREF_KEY, null);
+        if (storedValue == null) return null;
+        try {
+            Class<?> storedClass = Class.forName(storedValue);
+            if (SMSParser.class.isAssignableFrom(storedClass))
+                //The cast is safe due to the above check.
+                return (Class<? extends SMSParser>) storedClass;
+            else
+                return null;
+        } catch (ClassNotFoundException e) {
+            //The class might have been renamed or its name might have been inappropriately stored.
+            return null;
+        }
     }
 
     /**
      * Method resetting the parser this instance uses to the default (no operation on messages).
      *
      * @return The reference to the previously set {@link SMSParser} class. The Type of stored
-     * class is not questioned (as long as it extends {@link SMSParser}.
+     * class is not questioned (as long as it extends {@link SMSParser}).
      */
     @Nullable
     public Class<? extends SMSParser> resetOverridingParser() {
-        return null;
+        Class<? extends SMSParser> oldStoredClass = getOverridingParser();
+        storeInPreferences(null);
+        overridingParser = instantiateOverridingParser();
+        return oldStoredClass;
+    }
+
+    /**
+     * Method to create an instance of the Overriding parser class. If the class cannot be
+     * instantiated, the instance is left {@code null};
+     *
+     * @return The instance of the stored class, can be {@code null}.
+     */
+    @Nullable
+    private SMSParser instantiateOverridingParser() {
+        Class<? extends SMSParser> overridingClass = getOverridingParser();
+        if (overridingClass == null) return null;
+        try {
+            return overridingClass.newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            return null;
+        }
     }
 
     /**
@@ -94,7 +197,8 @@ public class SMSParsingManager implements SMSParser {
      */
     @Override
     public SMSMessage parseOutgoingMessage(SMSMessage message) {
-        return message;
+        if (overridingParser == null) return message;
+        return overridingParser.parseOutgoingMessage(message);
     }
 
     /**
@@ -104,6 +208,7 @@ public class SMSParsingManager implements SMSParser {
      */
     @Override
     public SMSMessage parseIncomingMessage(SMSMessage message) {
-        return message;
+        if (overridingParser == null) return message;
+        return overridingParser.parseIncomingMessage(message);
     }
 }
--- msglibrary/src/test/java/ingsw/group1/msglibrary/parsing/SMSParsingManagerTest.java
+++ msglibrary/src/test/java/ingsw/group1/msglibrary/parsing/SMSParsingManagerTest.java
@@ -1,10 +1,25 @@
 package ingsw.group1.msglibrary.parsing;
 
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import ingsw.group1.msglibrary.RandomSMSPeerGenerator;
 import ingsw.group1.msglibrary.SMSMessage;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Class containing tests for {@link SMSParsingManager}.
@@ -12,13 +27,82 @@ import ingsw.group1.msglibrary.SMSMessage;
  *
  * @author Riccardo De Zen
  */
+@Config(sdk = 28)
 @RunWith(RobolectricTestRunner.class)
 public class SMSParsingManagerTest {
 
     private static final String PREF_FILENAME = SMSParsingManager.PREF_FILENAME;
-    private static final String PREF_KEY = SMSParsingManager.PREF_KEY;
+    private static final String PREF_KEY = SMSParsingManager.PREF_KEY_PREFIX;
+    private static final String EXAMPLE_NAME = "I'm a name";
+    private static final String ANOTHER_NAME = "Another name";
+    private static final SMSMessage EXAMPLE_MESSAGE = new SMSMessage(
+            new RandomSMSPeerGenerator().generateValidPeer(),
+            "I'm an example message"
+    );
 
     private SMSParsingManager parsingManager;
+
+    private Context context;
+
+    /**
+     * Example parser to test the set and read feature in {@link SMSParsingManager}. It is
+     * relevant for this class to be a private child class because the way their name is written
+     * influences the reflection mechanisms used to instantiate the class.
+     */
+    private static class ExampleSMSParser implements SMSParser {
+
+        @Override
+        public SMSMessage parseOutgoingMessage(SMSMessage message) {
+            return new SMSMessage(message.getPeer(), "#" + message.getData());
+        }
+
+        @Override
+        public SMSMessage parseIncomingMessage(SMSMessage message) {
+            return new SMSMessage(message.getPeer(), message.getData().substring(1));
+        }
+    }
+
+    /**
+     * Another example parser, to test ability to get the previously stored value.
+     */
+    private static class AnotherExampleSMSParser implements SMSParser {
+
+        @Override
+        public SMSMessage parseOutgoingMessage(SMSMessage message) {
+            return new SMSMessage(message.getPeer(), "#" + message.getData());
+        }
+
+        @Override
+        public SMSMessage parseIncomingMessage(SMSMessage message) {
+            return new SMSMessage(message.getPeer(), message.getData().substring(1));
+        }
+    }
+
+    /**
+     * Method to initialize {@link Context} through {@link RobolectricTestRunner}.
+     */
+    @Before
+    public void initContext() {
+        context = ApplicationProvider.getApplicationContext();
+        parsingManager = SMSParsingManager.getInstance(context, EXAMPLE_NAME);
+    }
+
+    /**
+     * Method to reset the instances of the {@link SMSParsingManager} class, to reset Preferences
+     * as well.
+     */
+    @After
+    public void resetInstances() {
+        try {
+            Field instances = SMSParsingManager.class.getDeclaredField("activeInstances");
+            instances.setAccessible(true);
+            Map<?, ?> instancesMap = (Map<?, ?>) instances.get(null);
+            if (instancesMap != null) instancesMap.clear();
+            else throw new NoSuchFieldException("Instance Map was null");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException("Instances could not be reset due to: " + e.getMessage());
+        }
+    }
 
     /**
      * Test to assert the class works as a proper key-based Object Pool: same key means same
@@ -26,7 +110,10 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void sameNameInstancesAreSame() {
-
+        assertEquals(
+                SMSParsingManager.getInstance(context, EXAMPLE_NAME),
+                SMSParsingManager.getInstance(context, EXAMPLE_NAME)
+        );
     }
 
     /**
@@ -35,7 +122,10 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void differentNameInstancesAreDifferent() {
-
+        assertNotEquals(
+                SMSParsingManager.getInstance(context, EXAMPLE_NAME),
+                SMSParsingManager.getInstance(context, ANOTHER_NAME)
+        );
     }
 
     /**
@@ -44,7 +134,11 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void parseOutgoingDoesNothingByDefault() {
-
+        SMSMessage parsedMessage = parsingManager.parseOutgoingMessage(EXAMPLE_MESSAGE);
+        assertTrue(
+                EXAMPLE_MESSAGE.getPeer().equals(parsedMessage.getPeer()) &&
+                        EXAMPLE_MESSAGE.getData().equals(parsedMessage.getData())
+        );
     }
 
     /**
@@ -53,16 +147,36 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void parseIncomingDoesNothingByDefault() {
-
+        SMSMessage parsedMessage = parsingManager.parseIncomingMessage(EXAMPLE_MESSAGE);
+        assertTrue(
+                EXAMPLE_MESSAGE.getPeer().equals(parsedMessage.getPeer()) &&
+                        EXAMPLE_MESSAGE.getData().equals(parsedMessage.getData())
+        );
     }
 
     /**
+     * // FIXME: 19/01/2020 The preference is read as null from outside on separated tests.
      * Test to assert the class can write the parser class in
-     * {@link android.content.SharedPreferences}.
+     * {@link android.content.SharedPreferences} and subsequently retrieve it, when starting from
+     * a {@code null} value.
      */
     @Test
-    public void canSetParser() {
+    public void canSetAndGetParserFromNull() {
+        parsingManager.setOverridingParser(ExampleSMSParser.class);
+        assertEquals(ExampleSMSParser.class, parsingManager.getOverridingParser());
+    }
 
+    /**
+     * // FIXME: 19/01/2020 Same as above test.
+     * Test to assert the class can write the parser class in
+     * {@link android.content.SharedPreferences} and subsequently retrieve it, when starting from
+     * some other already set value.
+     */
+    @Test
+    public void canSetAndGetParserFromAlreadySet() {
+        parsingManager.setOverridingParser(ExampleSMSParser.class);
+        parsingManager.setOverridingParser(AnotherExampleSMSParser.class);
+        assertEquals(AnotherExampleSMSParser.class, parsingManager.getOverridingParser());
     }
 
     /**
@@ -71,23 +185,25 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void setParsingManagerReturnsOldValue() {
-
+        parsingManager.setOverridingParser(ExampleSMSParser.class);
+        Class<?> oldValue = parsingManager.setOverridingParser(AnotherExampleSMSParser.class);
+        assertEquals(ExampleSMSParser.class, oldValue);
     }
 
-    /**
-     * Test asserting the class can get the previously saved parser class.
-     */
-    @Test
-    public void canGetParser() {
-
-    }
+    //TODO reset tests
 
     /**
      * Test asserting the parser actually overrides for outgoing messages.
      */
     @Test
     public void isOutgoingParsingOverridden() {
-
+        parsingManager.setOverridingParser(ExampleSMSParser.class);
+        SMSMessage actualParsedMessage = parsingManager.parseOutgoingMessage(EXAMPLE_MESSAGE);
+        SMSMessage expectedParsedMessage = parsingManager.parseOutgoingMessage(EXAMPLE_MESSAGE);
+        assertTrue(
+                expectedParsedMessage.getPeer().equals(actualParsedMessage.getPeer()) &&
+                        expectedParsedMessage.getData().equals(actualParsedMessage.getData())
+        );
     }
 
     /**
@@ -95,6 +211,12 @@ public class SMSParsingManagerTest {
      */
     @Test
     public void isIncomingParsingOverridden() {
-
+        parsingManager.setOverridingParser(ExampleSMSParser.class);
+        SMSMessage actualParsedMessage = parsingManager.parseOutgoingMessage(EXAMPLE_MESSAGE);
+        SMSMessage expectedParsedMessage = parsingManager.parseOutgoingMessage(EXAMPLE_MESSAGE);
+        assertTrue(
+                expectedParsedMessage.getPeer().equals(actualParsedMessage.getPeer()) &&
+                        expectedParsedMessage.getData().equals(actualParsedMessage.getData())
+        );
     }
 }
